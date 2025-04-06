0691d99c3e4e933ee198858e5896295a81497eb9
Dong Chenxuan <3124861252@qq.com>
2025-04-06 13:05:45 +0800

    fix:实际的程序，但十分容易连不上github以及其他未知错误。需要好几分钟构建trace，很折磨

--- lean_demo/lean_validate.py
+++ lean_demo/lean_validate.py
@@ -1,9 +1,11 @@
 from lean_dojo import *
 
-local_repo_path = "C:\\Users\\Lenovo\\Desktop\\pythonlab\\lean-eval"
-local_file_path = "C:\\Users\\Lenovo\\Desktop\\pythonlab\\lean-eval\\data\\test.lean"
-repo = LeanGitRepo(local_repo_path , None)
-theorem = Theorem(repo, local_file_path, "add_comm")
+def main():
+  repo = LeanGitRepo("https://github.com/maksymilan/lean-eval" , "6bd0900d470f688ec876134fdd54406b2de4a36e")
+  theorem = Theorem(repo, "test.lean", "add_comm")
 
-with Dojo(theorem) as (dojo, init_state):
-  print(init_state)
+  with Dojo(theorem) as (dojo, init_state):
+    print(init_state)
+
+if __name__ == "__main__":
+  main()
